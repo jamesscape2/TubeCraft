0774c5b9ea659b4b3803b69076b385de47bc5605
James <Brooks>
2014-06-29 12:53:36 +0800

    Basic File Structure

---
+++ src/main/java/com/jamesscape2/TubeCraft/proxy/CommonProxy.java
@@ -0,0 +1,11 @@
+package com.jamesscape2.tubecraft.proxy;
+
+/**
+ * Project Name: TubeCraft
+ *
+ * Package Name: com.jamesscape2.tubecraft.proxy
+ *
+ * @author Jamesscape2
+ */
+public abstract class CommonProxy implements IProxy
+{}
---
+++ src/main/java/com/jamesscape2/TubeCraft/proxy/ServerProxy.java
@@ -0,0 +1,13 @@
+package com.jamesscape2.tubecraft.proxy;
+
+/**
+ * Project Name: TubeCraft
+ *
+ * Package Name: com.jamesscape2.tubecraft.proxy
+ *
+ * @author Jamesscape2
+ */
+public class ServerProxy extends CommonProxy
+{
+
+}
