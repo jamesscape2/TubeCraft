f29b528e2e1963be179d291e5017187552658088
James <Brooks>
2014-07-10 23:48:23 +0800

    Few slight changes to gradle.  hopefully the @VERSION@ stuff works

--- src/main/java/com/jamesscape2/tcraft/reference/Reference.java
+++ src/main/java/com/jamesscape2/tcraft/reference/Reference.java
@@ -11,7 +11,7 @@ public class Reference
 {
     public static final String MOD_ID = "tCraft";
     public static final String MOD_NAME = "Tube Craft";
-    public static final String VERSION = "1.7.10-1.0";
+    public static final String VERSION = "@VERSION@";
     public static final String SERVER_PROXY_CLASS = "com.jamesscape2.tcraft.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.jamesscape2.tcraft.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.jamesscape2.tcraft.client.gui.GuiFactory";
